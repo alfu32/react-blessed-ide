570573b62ce8c4a5053d68a7f217b6b1cc1bdcbe
alferio <ioan-cristian.alferaru@ec.europa.eu>
2025-08-01 11:43:36 +0200

    fix(synch): fix sync all script

--- git-sync-all.sh
+++ git-sync-all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 git pull origin work
-git push origin work
+git push origin work --tags
 git checkout staging
 git pull origin staging
 git merge --ff-only work
-git push origin staging
+git push origin staging --tags
 git checkout published
 git pull origin published
 git merge --ff-only work
-git push origin published
+git push origin published --tags
 git checkout work
